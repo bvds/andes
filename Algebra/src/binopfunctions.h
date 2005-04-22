41ba95bfa01d3987a0f57f2d2616a0bd688e177c
bvds <bvds>
2005-04-22 22:49:55 +0000

    put function prototypes in one place

---
+++ Algebra/src/binopfunctions.h
@@ -0,0 +1,17 @@
+#ifndef BINOPFUNCTIONS_INCLUDED
+#define BINOPFUNCTIONS_INCLUDED
+#include "expr.h"
+#include "decl.h"
+//
+//  Associated function prototypes
+// 
+
+void recassign( vector<binopexp *> * & eqn, vector<varindx> * & vars,
+		vector<binopexp *> * soleqs);		       // recassign.cpp
+vector<binopexp *> * dopurelin(vector<binopexp *> * & eqn, 	// purelin.cpp
+       vector<varindx> * & vars, vector<binopexp *> * soleqs,
+			       int & doagain );
+bool dofactor(vector<binopexp *> * & eqn, vector<varindx> * & vars);
+bool donlsolv(vector<binopexp *> * & eqn);			// donlsolv.cpp
+int dotrig(vector<binopexp *> * & eqn);			 // dotrig.cpp
+#endif
