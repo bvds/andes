fd4f2f2d90d0892426ef0ddcb5e3ee7081414c22
bvds <bvds>
2006-07-07 17:56:52 +0000

    allow $m0 and $e0 along with mu0 and eps0

--- Algebra/src/pconsts.h
+++ Algebra/src/pconsts.h
@@ -7,8 +7,10 @@
 //
 physc("$P",     3.14159265358979323, 0, 0, 0, 0, 0), // pi
 physc("Pi",     3.14159265358979323, 0, 0, 0, 0, 0), // pi
+physc("$e0",       8.854187817E-12,-3,-1, 2, 2, 0), // pemittivity of vacuum
 physc("eps0",       8.854187817E-12,-3,-1, 2, 2, 0), // pemittivity of vacuum
 physc("kelec",  8.98755178799791E+9, 3, 1,-2,-2, 0), // 1/4*pi*eps0
+physc("$m0",  1.2566370614359173E-6, 1, 1, 0,-2, 0), // permeability of vacuum
 physc("mu0",  1.2566370614359173E-6, 1, 1, 0,-2, 0), // permeability of vacuum
 physc("kmag",                1.0E-7, 1, 1, 0,-2, 0), // mu0/(4*pi)
 physc("c",                299792458, 1, 0,-1, 0, 0), // speed of light
