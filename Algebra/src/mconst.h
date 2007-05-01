e9d6b74d7089bd6968a8a2c9148b7824d3b6b0a4
bvds <bvds>
2007-05-01 16:32:11 +0000

    M_PI sometimes already defined

--- Algebra/src/mconst.h
+++ Algebra/src/mconst.h
@@ -1,6 +1,7 @@
-#define junk
 // Copyright (C) 2001 by Joel A. Shapiro -- All Rights Reserved
 
+#ifndef M_PI  // sometimes defined in math.h
 #define M_PI		3.14159265358979323846
+#endif
 const double DEGTORAD =  M_PI/180.0;
 const double RADTODEG =  180.0/M_PI;
