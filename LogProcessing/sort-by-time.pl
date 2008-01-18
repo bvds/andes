b3f9923a2febb3dbb8889803a81044d63c14dff9
bvds <bvds>
2008-01-18 20:59:17 +0000

    undo use of timestamp from local computer, less accurate than OLI timestamp

--- LogProcessing/sort-by-time.pl
+++ LogProcessing/sort-by-time.pl
@@ -3,19 +3,10 @@
 #  files containing Andes logs, as gotten from OLI, 
 #  are not sorted by time.
 #  This script sorts sessions in a log file by time
-# Had to install this on FC6.
-# On OS X, run existing cpan and install DateTime::Format::Strptime
-use DateTime::Format::Strptime;
-my $dateformat =
-    DateTime::Format::Strptime->new( pattern => '%B %d, %Y %T' );
-
-$date=DateTime->new( year => 1000 );    # Any lines before the first session 
-
+$this="";       # Any lines before the first session 
 while (<>) {
-    if(/^.* Log of Andes session begun \w+, (.+) by /) {
-      $date = $dateformat->parse_datetime($1);
-    }
-    $sessions{$date} .= $_;
+    if(/^.* Log of Andes session begun/) {$this=$_;}
+    $sessions{$this} .= $_;
 }
 foreach $session (sort keys %sessions) {
     print "$sessions{$session}";
