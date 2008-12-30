41d2ecab0853384d6a8f514c663fddcf0be669a1
Brett van de Sande <bvds@asu.edu>
2008-12-30 13:06:58 -0700

    remove Workbench code and Allegro-lisp specific code

--- Setup/Files.h
+++
@@ -1,9 +0,0 @@
-// 
-// Final sets of all files and directories to install:
-//
-extern CStringArray directories;
-extern CStringArray files;
-
-// Init lists by adding files and dirs from problem list
-extern void BuildFileList();
-
