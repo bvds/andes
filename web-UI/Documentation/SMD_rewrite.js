dc6406be24265097df3e6638f188b2cfbb2aa241
Mike Wilcox <mwilcox@sitepen.com>
2009-05-02 17:50:42 -0500

    Adding UI Designs for choosing colors, button styles.

--- web-UI/Documentation/SMD_rewrite.js
+++ web-UI/Documentation/SMD_rewrite.js
@@ -1,3 +1,4 @@
+var foo = // this line is here so my IDE error-checker will work
 {
 	"envelope": "JSON-RPC-2.0",
 	"transport": "POST",
@@ -14,17 +15,21 @@
 			],
 			"returns":{
 				"type": "object",
+				"score": {
+					"type": ["object","number"],
+					"optional": true
+				},
 				"items":{
 					"type": "array",
 					"item": {
 						"type": "object",
 						"id": {
-							"description": "Identifier for each drawn item, set by the creator of the object.  Used only by actions new-object, modify-object, and delete-object",
+							"description": "Identifier for each drawn item, set by the creator of the object.",
 							"type": "string", 
 							"optional": true
 						},
 						"type": {
-							"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object",
+							"description": "The kind of drawn object",
 							"type": "string",
 							"enum": [
 								"text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"
@@ -32,56 +37,22 @@
 							"optional": true
 						},
 						"mode": {
-							"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable",
+							"description": "The state of the item. unknown: black; correct: green; wrong: red; locked: black, not user selectable; fade: gray, not user selectable",
 							"type": "string",
 							"enum": ["unknown","right","wrong","locked","fade"],
 							"optional": true
 						},
-						"x": {
-							"type": "number",
-							"optional": true
-						},
-						"y": {
-							"type": "number",
-							"optional": true
-						},
-						"width": {
-							"type": "integer",
-							"optional": true
-						},
-						"height": {
-							"type": "integer",
-							"optional": true
-						},
-						"text":{
-							"type": "string",
-							"optional": true
-						},
-						"radius":{
-							"type": "number",
-							"optional": true
-						},
-						"symbol": {
-							"type": "string",
-							"optional": true
-						},
-						"x-label":{
-							"type": "string",
-							"optional": true
-						},
-						"y-label":{
-							"type": "string",
-							"optional": true
-						},
-						"angle": {
-							"type": "number",
-							"optional": true
-						}
+						"x": 		{"type": "number",	"optional": true, "description": "The x location of item"},
+						"y": 		{"type": "number",	"optional": true, "description": "The y location of item"},
+						"width": 	{"type": "integer",	"optional": true, "description": "Width of item"},
+						"height": 	{"type": "integer",	"optional": true, "description": "Height of item"},
+						"text":		{"type": "string",	"optional": true, "description": "Text content of item"},
+						"radius":	{"type": "number",	"optional": true, "description": "Radius of item"},
+						"symbol": 	{"type": "string",	"optional": true, "description": "Variable-name of item"},
+						"x-label":	{"type": "string",	"optional": true, "description": "The x-axis label of an axes"},
+						"y-label":	{"type": "string",	"optional": true, "description": "The x-axis label of an axes"},
+						"angle": 	{"type": "number",	"optional": true, "description": "The angle of an item"}
 					}
-				},
-				"score": {
-					"type": ["object","number"],
-					"optional": true
 				}
 			}
 		},
@@ -93,48 +64,67 @@
 					"enum":["new-object","modify-object", "delete-object"]
 				},
 				{
-					"name": "id",
-					"type": "string",
-					"optional": true,
-					"description": "Identifier for each drawn item, set by the creator of the object. Used only by actions new-object, modify-object, and delete-object"
-				},
-				{
-					"name": "type",
-					"type": "string",
-					"enum": ["text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"],
-					"optional": true,
-					"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object"
-				},
-				{
-					"name": "mode",
-					"type": "string",
-					"enum": ["unknown","right","wrong","locked","fade"],
-					"optional": true,
-					"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable"
-				},
-				{"name": "x", "type": "number", "optional": true},
-				{"name": "y", "type": "number", "optional": true},
-				{"name": "width", "type": "integer", "optional": true}, 
-				{"name": "height", "type": "integer", "optional": true},
-				{"name": "text", "type": "string", "optional": true},
-				{"name": "radius", "type": "number", "optional": true},
-				{"name": "symbol", "type": "string", "optional": true},
-				{"name": "x-label", "type": "string", "optional": true},
-				{"name": "y-label", "type": "string", "optional": true},
-				{"name": "angle", "type": "number", "optional": true}
+					"name":"items",
+					"type":"array",
+					"parameters": [
+						{
+							"name":"item",
+							"type":"object"
+						},
+						{
+							"name": "id",
+							"type": "string",
+							"optional": true,
+							"description": "Identifier for each drawn item, set by the creator of the object. Used only by actions new-object, modify-object, and delete-object"
+						},
+						{
+							"name": "type",
+							"type": "string",
+							"enum": ["text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"],
+							"optional": true,
+							"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object"
+						},
+						{
+							"name": "mode",
+							"type": "string",
+							"enum": ["unknown","right","wrong","locked","fade"],
+							"optional": true,
+							"description": "The state of the item. unknown: black; correct: green; wrong: red; locked: black, not user selectable; fade: gray, not user selectable"
+						},
+						{"name": "x", 		"type": "number", 	"optional": true},
+						{"name": "y", 		"type": "number", 	"optional": true},
+						{"name": "width", 	"type": "integer", 	"optional": true}, 
+						{"name": "height", 	"type": "integer", 	"optional": true},
+						{"name": "text", 	"type": "string", 	"optional": true},
+						{"name": "radius", 	"type": "number",	"optional": true},
+						{"name": "symbol", 	"type": "string", 	"optional": true},
+						{"name": "x-label", "type": "string", 	"optional": true},
+						{"name": "y-label", "type": "string", 	"optional": true},
+						{"name": "angle", 	"type": "number", 	"optional": true}
+					]
+				}
 			],
+			
 			"returns":{
 				"type": "object",
 				"score": {
 					"type": ["object","number"],
 					"optional": true
 				},
-				"mode": {
-					"type": "string",
-					"enum": ["unknown","right","wrong","locked","fade"],
-					"optional": true,
-					"description": "response for new-object, modify-object or delete-object.\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable"
+				"items":{
+					"type":"array",
+					"item":{
+						type:"array",
+						"id": {
+							"type": "string"
+						},
+						"mode": {
+							"type": "string",
+							"enum": ["unknown","right","wrong","locked","fade"]
+						}
+					}
 				}
+			}
 		},
 		"seek-help": {
 			"parameters": [
@@ -143,10 +133,17 @@
 					"type": "string",
 					"enum":["get-help","help-button","principles-menu"]
 				},
-				{"name": "href", "type": "string", "optional": true},
-				{"name": "value", "type": "string", "optional": true},
-				{"name": "text", "type": "string", "optional": true}
-			]
+				{"name": "href", 	"type": "string", "optional": true},
+				{"name": "value", 	"type": "string", "optional": true},
+				{"name": "text", 	"type": "string", "optional": true}
+			],
+			"returns":{
+				"type": "object",
+				"helpContent":{
+					"type":"string"
+				}
+				//MAY NEED MORE PARAMS HERE, MULTI CHOICE, HINTS, ETC
+			}
 		},
 		"close-problem": {
 			"parameters": [
@@ -156,55 +153,6 @@
 					"optional": true
 				}
 			]
-		}
-	},
-	
-	"returns": {
-		"type": "array",
-		"items": {
-			"type": "object",
-			"properties": {
-				"action": {
-					"type": "string",
-					"description": "The choice get-help is only from the  client while\nthe choices set-score, show-hint, show-hint-link, and focus-hint-text-box are only from the server. \nLog actions are not read by the client and my be stripped from any response sent to the client. This is just a first attempt at the client-server API, any suggestions are welcome.",
-					"enum":[
-							"new-object",
-							"modify-object",
-							"delete-object",
-							"log",
-							"get-help",
-							"help-button",
-							"set-score",
-							"show-hint",
-							"show-hint-link",
-							"focus-hint-text-box"
-					]
-				},
-			"id": {
-				"description": "Identifier for each drawn item, set by the creator of the object.  Used only by actions new-object, modify-object, and delete-object",
-				"type": "string",  //CHANGED - was number
-				"optional": true
-			},
-			"type": {
-				"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object",
-				"type": "string",
-				"enum": [
-					"text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"
-				],
-				"optional": true
-			},
-			"mode": {
-				"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable",
-				"type": "string",
-				"enum": ["unknown","right","wrong","locked","fade"],
-				"optional": true
-			},
-			"score": {"type": ["object","number"], "optional": true}
-			
-			
-			// on startup, this needs to return everything from above
-			
-			
 		}
 	}
 }}
