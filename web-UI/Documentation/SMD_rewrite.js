66ac6fe7bf9957d1d27ba3115a2ab89efb473686
Mike Wilcox <mwilcox@sitepen.com>
2009-05-03 06:43:36 -0500

    Adding SMD_rewrite.js for client review

--- web-UI/Documentation/SMD_rewrite.js
+++ web-UI/Documentation/SMD_rewrite.js
@@ -1,4 +1,4 @@
-var foo = // this line is here so my IDE error-checker will work
+var foo = // this line is here so my IDE parser works with json
 {
 	"envelope": "JSON-RPC-2.0",
 	"transport": "POST",
@@ -14,48 +14,54 @@ var foo = // this line is here so my IDE error-checker will work
 				{"name": "problem", "type": "string"}
 			],
 			"returns":{
-				"type": "object",
-				"score": {
-					"type": ["object","number"],
-					"optional": true
-				},
-				"items":{
-					"type": "array",
-					"item": {
-						"type": "object",
-						"id": {
-							"description": "Identifier for each drawn item, set by the creator of the object.",
-							"type": "string", 
-							"optional": true
-						},
-						"type": {
-							"description": "The kind of drawn object",
-							"type": "string",
-							"enum": [
-								"text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"
-							],
-							"optional": true
-						},
-						"mode": {
-							"description": "The state of the item. unknown: black; correct: green; wrong: red; locked: black, not user selectable; fade: gray, not user selectable",
-							"type": "string",
-							"enum": ["unknown","right","wrong","locked","fade"],
-							"optional": true
-						},
-						"x": 		{"type": "number",	"optional": true, "description": "The x location of item"},
-						"y": 		{"type": "number",	"optional": true, "description": "The y location of item"},
-						"width": 	{"type": "integer",	"optional": true, "description": "Width of item"},
-						"height": 	{"type": "integer",	"optional": true, "description": "Height of item"},
-						"text":		{"type": "string",	"optional": true, "description": "Text content of item"},
-						"radius":	{"type": "number",	"optional": true, "description": "Radius of item"},
-						"symbol": 	{"type": "string",	"optional": true, "description": "Variable-name of item"},
-						"x-label":	{"type": "string",	"optional": true, "description": "The x-axis label of an axes"},
-						"y-label":	{"type": "string",	"optional": true, "description": "The x-axis label of an axes"},
-						"angle": 	{"type": "number",	"optional": true, "description": "The angle of an item"}
+				"properties":{
+					"score":{
+						"type": ["object","number"],
+						"optional": false
+					},
+					"items":{
+						"type":"array",
+						"items":[
+							{
+								"type":"object",
+								"properties":{
+									"id":{
+										"type": "string",
+										"optional": false,
+										"description": "Identifier for each drawn item, set by the creator of the object."
+									},
+									"type":{
+										"type": "string",
+										"enum": ["text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"],
+										"optional": true,
+										"description": "kind of drawn object"
+									},
+									"mode":{
+										"type": "string",
+										"enum": ["unknown","right","wrong","locked","fade"],
+										"optional": true,
+										"description": "State of the item; unknown: black; correct: green; wrong: red; locked: black(not user selectable); fade: gray (not user selectable)"
+									},
+									"x":		{"type": "number"},
+									"y":		{"type": "number"},
+									"width":	{"type": "number",  "optional": true}, 
+									"height":	{"type": "number",	"optional": true},
+									"text":		{"type": "string", 	"optional": true},
+									"radius":	{"type": "number", 	"optional": true},
+									"symbol":	{"type": "string", 	"optional": true},
+									"x-label":	{"type": "string", 	"optional": true},
+									"y-label":	{"type": "string", 	"optional": true},
+									"angle":	{"type": "number", 	"optional": true}
+								}
+							}
+						]
 					}
 				}
 			}
 		},
+		
+		
+		
 		"solution-step": {
 			"parameters": [
 				{
@@ -64,68 +70,69 @@ var foo = // this line is here so my IDE error-checker will work
 					"enum":["new-object","modify-object", "delete-object"]
 				},
 				{
-					"name":"items",
-					"type":"array",
-					"parameters": [
-						{
-							"name":"item",
-							"type":"object"
-						},
-						{
-							"name": "id",
-							"type": "string",
-							"optional": true,
-							"description": "Identifier for each drawn item, set by the creator of the object. Used only by actions new-object, modify-object, and delete-object"
-						},
-						{
-							"name": "type",
-							"type": "string",
-							"enum": ["text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"],
-							"optional": true,
-							"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object"
-						},
-						{
-							"name": "mode",
-							"type": "string",
-							"enum": ["unknown","right","wrong","locked","fade"],
-							"optional": true,
-							"description": "The state of the item. unknown: black; correct: green; wrong: red; locked: black, not user selectable; fade: gray, not user selectable"
-						},
-						{"name": "x", 		"type": "number", 	"optional": true},
-						{"name": "y", 		"type": "number", 	"optional": true},
-						{"name": "width", 	"type": "integer", 	"optional": true}, 
-						{"name": "height", 	"type": "integer", 	"optional": true},
-						{"name": "text", 	"type": "string", 	"optional": true},
-						{"name": "radius", 	"type": "number",	"optional": true},
-						{"name": "symbol", 	"type": "string", 	"optional": true},
-						{"name": "x-label", "type": "string", 	"optional": true},
-						{"name": "y-label", "type": "string", 	"optional": true},
-						{"name": "angle", 	"type": "number", 	"optional": true}
-					]
-				}
-			],
-			
-			"returns":{
-				"type": "object",
-				"score": {
-					"type": ["object","number"],
-					"optional": true
+					"name": "id",
+					"type": "string",
+					"optional": true,
+					"description": "Identifier for each drawn item, set by the creator of the object. Used only by actions new-object, modify-object, and delete-object"
 				},
-				"items":{
-					"type":"array",
-					"item":{
-						type:"array",
-						"id": {
-							"type": "string"
-						},
-						"mode": {
-							"type": "string",
-							"enum": ["unknown","right","wrong","locked","fade"]
-						}
+				{
+					"name": "type",
+					"type": "string",
+					"enum": ["text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"],
+					"optional": true,
+					"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object"
+				},
+				{
+					"name": "mode",
+					"type": "string",
+					"enum": ["unknown","right","wrong","locked","fade"],
+					"optional": true,
+					"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable"
+				},
+				{"name": "x", 		"type": "number", 	"optional": true},
+				{"name": "y",	 	"type": "number", 	"optional": true},
+				{"name": "width", 	"type": "integer", 	"optional": true}, 
+				{"name": "height", 	"type": "integer",	"optional": true},
+				{"name": "text", 	"type": "string", 	"optional": true},
+				{"name": "radius", 	"type": "number", 	"optional": true},
+				{"name": "symbol", 	"type": "string", 	"optional": true},
+				{"name": "x-label", "type": "string", 	"optional": true},
+				{"name": "y-label", "type": "string", 	"optional": true},
+				{"name": "angle", 	"type": "number", 	"optional": true}
+			],
+			"returns":{
+				"parameters": [
+					{
+						"name":"score",
+						"type": ["object","number"],
+						"optional": true
+					},
+					{
+						"name":"items",
+						"type":"array",
+						"items":[
+							{
+								"name":"item",
+								"type":"object"
+							},
+							{
+								"name": "id",
+								"type": "string"
+							},
+							{
+								"name": "mode",
+								"type": "string",
+								"enum": ["unknown","right","wrong","locked","fade"],
+								"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable"
+							}
+							
+						]
 					}
-				}
+				]
 			}
 		},
+		
+		
 		"seek-help": {
 			"parameters": [
 				{
@@ -133,18 +140,29 @@ var foo = // this line is here so my IDE error-checker will work
 					"type": "string",
 					"enum":["get-help","help-button","principles-menu"]
 				},
-				{"name": "href", 	"type": "string", "optional": true},
-				{"name": "value", 	"type": "string", "optional": true},
-				{"name": "text", 	"type": "string", "optional": true}
+				{"name": "href", "type": "string", "optional": true},
+				{"name": "value", "type": "string", "optional": true},
+				{"name": "text", "type": "string", "optional": true}
 			],
 			"returns":{
-				"type": "object",
-				"helpContent":{
-					"type":"string"
-				}
-				//MAY NEED MORE PARAMS HERE, MULTI CHOICE, HINTS, ETC
+				"parameters": [
+					{
+						// NOTE - DON'T KNOW IF THIS IS RIGHT SINCE I DON'T KNOW WHAT 'HELP' RETURNS
+						"name":"content",
+						"type":"string",
+						"enum":[
+							"help-button",
+							"set-score",
+							"show-hint",
+							"show-hint-link",
+							"focus-hint-text-box"
+						]
+					}
+				]
 			}
 		},
+		
+		
 		"close-problem": {
 			"parameters": [
 				{
@@ -152,7 +170,63 @@ var foo = // this line is here so my IDE error-checker will work
 					"type": "string",
 					"optional": true
 				}
-			]
+			],
+			"returns":{
+				// NOTE - DON'T KNOW IF THIS IS RIGHT - DOES CLOSE RETURN ANYTHING?
+				"name":"success",
+				"type": "boolean"
+			}
+		}
+	},
+	
+	
+	// LEFT FOR REFERENCE - DISREGARD:
+	"returns": {
+		"type": "array",
+		"items": {
+			"type": "object",
+			"properties": {
+				"action": {
+					"type": "string",
+					"description": "The choice get-help is only from the  client while\nthe choices set-score, show-hint, show-hint-link, and focus-hint-text-box are only from the server. \nLog actions are not read by the client and my be stripped from any response sent to the client. This is just a first attempt at the client-server API, any suggestions are welcome.",
+					"enum":[
+							"new-object",
+							"modify-object",
+							"delete-object",
+							"log",
+							"get-help",
+							"help-button",
+							"set-score",
+							"show-hint",
+							"show-hint-link",
+							"focus-hint-text-box"
+					]
+				},
+			"id": {
+				"description": "Identifier for each drawn item, set by the creator of the object.  Used only by actions new-object, modify-object, and delete-object",
+				"type": "string",  //CHANGED - was number
+				"optional": true
+			},
+			"type": {
+				"description": "kind of drawn object; manditory for new-object and optional for modify-object or delete-object",
+				"type": "string",
+				"enum": [
+					"text", "graphics", "equation", "circle", "rectangle", "axes", "vector", "line"
+				],
+				"optional": true
+			},
+			"mode": {
+				"description": "manditory for new-object and optional for modify-object or delete-object\n  unknown:  turn black\n  correct:  turn green\n  wrong:  turn red\n  locked:  black, not user selectable\n  fade:  gray, not user selectable",
+				"type": "string",
+				"enum": ["unknown","right","wrong","locked","fade"],
+				"optional": true
+			},
+			"score": {"type": ["object","number"], "optional": true}
+			
+			
+			// on startup, this needs to return everything from above
+			
+			
 		}
 	}
 }}
