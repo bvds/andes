7fb787fc5071529042e33b9c6322c1dfdc42852a
Mike Wilcox <mwilcox@sitepen.com>
2009-06-29 14:12:43 -0500

    Refs #3217 - Implemented scrolling canvas. Also added a Pan tool. This still needs a bit of work - refactoring, and turned into a plugin. I'd like it to be a little more configurable and optional.

---
+++ web-UI/js/drawing/plugins/ScrollingCanvas.js
@@ -0,0 +1,9 @@
+dojo.provide("drawing.plugins.ScrollingCanvas");
+
+drawing.plugins.ScrollingCanvas = drawing.util.oo.declare(
+	function(){
+		console.warn("drawing.plugins.ScrollingCanvas")
+	},{
+		
+	}
+);
