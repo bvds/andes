684ade51d12ae5d72c641bac36140d0904ea96c2
Revin Guilen <revin@sitepen.com>
2009-07-08 13:04:24 -0700

    Refs #3223, Changes to the custom ExpandoPane to make for nicer open/close toggling.

--- web-UI/js/andes/widget/ExpandoPane.js
+++ web-UI/js/andes/widget/ExpandoPane.js
@@ -3,8 +3,27 @@ dojo.provide("andes.widget.ExpandoPane");
 dojo.require("dojox.layout.ExpandoPane");
 
 dojo.declare("andes.widget.ExpandoPane", dojox.layout.ExpandoPane, {
-	postCreate: function(){
+	startup: function(){
 		this.inherited(arguments);
-		dojo.style(this.domNode, "overflow", "visible");
+		this._openButtonNode = dojo.create("div", {id:"helpPaneOpenButton", innerHTML:"&lt;&lt;"});
+		dojo.style(this._openButtonNode, "display", "none");
+		dojo.place(this._openButtonNode, dojo.byId("drawingPane"), "last");
+		dojo.connect(this._openButtonNode, "onclick", this, "toggle");
+	},
+
+	_setupAnims: function(){
+		this._closedSize = 0;
+		this.inherited(arguments);
+	},
+
+	_showEnd: function(){
+		dojo.style(this._openButtonNode, "display", "none");
+		this.inherited(arguments);
+	},
+
+	_hideEnd: function(){
+		this.inherited(arguments);
+		dojo.style(this._openButtonNode, {display:"block", opacity:0});
+		dojo.fadeIn({node:this._openButtonNode}).play();
 	}
 });
