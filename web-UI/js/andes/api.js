c137427a1144ee9f0d455db5cf1f657aec760ee2
Revin Guilen <revin@sitepen.com>
2009-07-10 12:12:42 -0700

    Refs #3223, adding a comment to the API code, for clarity.

--- web-UI/js/andes/api.js
+++ web-UI/js/andes/api.js
@@ -108,6 +108,8 @@ dojo.require("andes.error");
 			console.warn("andes.api.step", params);
 			var dfd = queueRequest("solution-step", params);
 			dfd.addCallback(function(result){
+				// look for help embedded in the returned result, so we can
+				// queue it up in case the user opens the Help pane
 				andes.help.processStep(result);
 			});
 			return dfd;
