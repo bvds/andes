ece35a1c8277ab1fec49d27265d62edbef360658
nvaidyan <nvaidyan@asu.edu>
2009-09-16 13:40:15 -0700

    'Fixed database creation scripts, added jar'

--- LogProcessing/JavaDataShopExport/databaseCreationScripts/create_CLASS_INFORMATION.sql
+++ LogProcessing/JavaDataShopExport/databaseCreationScripts/create_CLASS_INFORMATION.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `CLASS_INFORMATION` (
-  `classID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `classID` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(45) NOT NULL,
   `school` varchar(45) NOT NULL,
   `period` varchar(45) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE `CLASS_INFORMATION` (
   `instructorName` varchar(50) NOT NULL,
   `schoolyearInfo` varchar(50) NOT NULL,
   `datasetID` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`classID`),
+  PRIMARY KEY  (`classID`),
   KEY `FK_classinformation_dataset` (`datasetID`),
   CONSTRAINT `FK_classinformation_dataset` FOREIGN KEY (`datasetID`) REFERENCES `STUDENT_DATASET` (`datasetID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='A table for class information'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='A table for class information'
--- LogProcessing/JavaDataShopExport/databaseCreationScripts/create_PROBLEM_ATTEMPT_TRANSACTION.sql
+++ LogProcessing/JavaDataShopExport/databaseCreationScripts/create_PROBLEM_ATTEMPT_TRANSACTION.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `PROBLEM_ATTEMPT_TRANSACTION` (
-  `tID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tID` int(10) unsigned NOT NULL auto_increment,
   `clientID` varchar(50) NOT NULL,
   `command` text NOT NULL,
   `initiatingParty` enum('client','server') NOT NULL,
-  PRIMARY KEY (`tID`,`clientID`) USING BTREE,
+  PRIMARY KEY  USING BTREE (`tID`,`clientID`),
   KEY `FK_transaction_problemstate` (`clientID`),
   CONSTRAINT `FK_transaction_problemstate` FOREIGN KEY (`clientID`) REFERENCES `PROBLEM_ATTEMPT` (`clientID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='each attempt has a series of transactions associated with it'
+) ENGINE=InnoDB AUTO_INCREMENT=1644 DEFAULT CHARSET=latin1 COMMENT='each attempt has a series of transactions associated with it'
