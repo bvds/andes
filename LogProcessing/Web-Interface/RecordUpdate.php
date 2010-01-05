dd631b056e9020c9b2b643b905eb87b72931891b
Rajagopalan Ranganathan <rrangan5@asu.edu>
2010-01-04 17:03:49 -0700

    Added changes for 'Check-Box' Updates.

--- LogProcessing/Web-Interface/RecordUpdate.php
+++ LogProcessing/Web-Interface/RecordUpdate.php
@@ -11,20 +11,28 @@ mysql_select_db($dbname)
 
 $adminName = $_GET["a"];
 $tID = $_GET["t"];
+$userName=$_GET["u"];
 
 $maxQuery = "SELECT MAX(radioID) FROM REVIEWED_PROBLEMS WHERE adminName='$adminName' AND tID=$tID";
 $maxResult = mysql_query($maxQuery);
 $maxResultArray = mysql_fetch_array($maxResult);
 $maxValue = $maxResultArray["MAX(radioID)"];
-
+if($maxValue == null)
+  {
+    $reviewQuery="INSERT INTO REVIEWED_PROBLEMS(userName,tID,adminName) VALUES('$userName',$tID,'$adminName')";
+    mysql_query($reviewQuery);   
+  }
+$maxResult = mysql_query($maxQuery);
+$maxResultArray = mysql_fetch_array($maxResult);
+$maxValue = $maxResultArray["MAX(radioID)"];
 if($maxValue == 0)
 $sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=1 WHERE adminName='$adminName' AND tID=$tID";
  else
    $sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=0 WHERE adminName='$adminName' AND tID=$tID";
-
+ 
  $result=mysql_query($sqlQuery);
 if($result == 1){
-  echo "Updated Record Successfully";
+  echo "Updated the Record Successfully";
  } else {
   echo "Error Updating Record";
  }
