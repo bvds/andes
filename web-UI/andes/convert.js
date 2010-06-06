1d99463eb87be786440160451f202ef63c083340
Ace Sorensen <acey@acey.local>
2010-06-05 19:54:16 -0700

    Updated convert to use data.cosphi

--- web-UI/andes/convert.js
+++ web-UI/andes/convert.js
@@ -270,7 +270,7 @@ dojo.provide("andes.convert");
 			}
 			
 			if(type=="vector" || type=="axes"){
-				obj.cosphi = item.cosphi;	
+				obj.cosphi = item.data.cosphi===undefined ? 0 : item.data.cosphi;	
 			}
 
 			if(combo){
