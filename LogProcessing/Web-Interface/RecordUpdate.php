8791ccc6e3c75345b659535c70f242c23f0a1d30
Brett van de Sande <bvds@asu.edu>
2010-06-19 21:47:17 -0700

    Fixes by rrangan that weren't committed.

--- LogProcessing/Web-Interface/RecordUpdate.php
+++ LogProcessing/Web-Interface/RecordUpdate.php
@@ -12,6 +12,8 @@ mysql_select_db($dbname)
 $adminName = $_GET["a"];
 $tID = $_GET["t"];
 $userName=$_GET["u"];
+$url=$_GET["c"];
+$comm=explode("&c=",$url);
 
 $maxQuery = "SELECT MAX(radioID) FROM REVIEWED_PROBLEMS WHERE adminName='$adminName' AND tID=$tID";
 $maxResult = mysql_query($maxQuery);
@@ -26,13 +28,13 @@ $maxResult = mysql_query($maxQuery);
 $maxResultArray = mysql_fetch_array($maxResult);
 $maxValue = $maxResultArray["MAX(radioID)"];
 if($maxValue == 0)
-$sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=1 WHERE adminName='$adminName' AND tID=$tID";
+$sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=1,myComment='$url' WHERE adminName='$adminName' AND tID=$tID";
  else
-   $sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=0 WHERE adminName='$adminName' AND tID=$tID";
+   $sqlQuery="UPDATE REVIEWED_PROBLEMS SET radioID=0,myComment='$url' WHERE adminName='$adminName' AND tID=$tID";
  
  $result=mysql_query($sqlQuery);
 if($result == 1){
-  echo "Updated the Record Successfully";
+  echo "Updated the Record Successflly";
  } else {
   echo "Error Updating Record";
  }
