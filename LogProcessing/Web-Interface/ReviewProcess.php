e0ffb1c4d09a12017f7e3a50d380b76d96c9cec6
Brett van de Sande <bvds@asu.edu>
2010-07-16 18:52:18 -0700

    Extend ReviewSolutions to be a general log browser.  Added JSON package to php.

--- LogProcessing/Web-Interface/ReviewProcess.php
+++ LogProcessing/Web-Interface/ReviewProcess.php
@@ -1,10 +1,11 @@
 <?
-$dbuser= "root";
+$dbuser= $_POST['dbuser'];
 $dbserver= "localhost";
 $dbpass= $_POST['passwd'];
 $dbname= "andes3";
 
-//CONNECTION STRING                                                                                                                                                                                                                                                       
+//CONNECTION STRING  
+    
 mysql_connect($dbserver, $dbuser, $dbpass)
      or die ("UNABLE TO CONNECT TO DATABASE");
 mysql_select_db($dbname)
@@ -13,42 +14,135 @@ mysql_select_db($dbname)
 $adminName = $_POST['adminName'];
 $orderBy = $_POST['item'];
 $order = $_POST['order'];
+$extra = $_POST['extra'];
+$slice = $_POST['slice'];
+$startDate = $_POST['startDate'];
+$endDate = $_POST['endDate'];
 
-echo "<h2>The Problems reviewed by you($adminName), sorted in $order order of $orderBy, are as follows:</h2><BR>";
 if($order=='Descending')
   $order = "DESC";
  else
    $order = "";
 
+if($slice == 'Comments'){
+  
+  if($adminName==''){
+    $adminNamec = "";
+    $adminNamee = "";
+  } else {
+    $adminNamec = "P1.userName = '$adminName' AND";
+    $adminNamee = " by $adminName";
+  }  
+  if($extra == 'Reviewed'){
+    $extrac = "P1.extra != 0 AND";
+    $extrae = "reviewed";
+  }else if($extra == 'Original'){
+    $extrac = "P1.extra = 0 AND";
+    $extrae = "solved";
+  }else{
+    $extrac = "";
+    $extrae = "solved or reviewed";
+  }
+  if($startDate){
+    $startDatec = "P1.startTime >= '$startDate' AND";
+  } else {
+    $startDatec = "";
+  }
+  if($endDate){
+    $endDatec = "P1.startTime <= '$endDate' AND";
+  } else {
+    $endDatec = "";
+  }
 
-$sql = "SELECT * FROM PROBLEM_ATTEMPT AS P1,PROBLEM_ATTEMPT_TRANSACTION AS P2 WHERE P1.userName = '$adminName' AND P1.extra !=0 AND P1.clientID = P2.clientID AND P2.initiatingParty = 'client' AND P2.command LIKE '%\"action\":\"get-help\",\"text\":%' ORDER BY $orderBy $order";
 
-$result = mysql_query($sql);
-if ($myrow = mysql_fetch_array($result)) {
-  echo "<table border=1>";
-  echo "<tr><th>User Name</th><th>Problem</th><th>Section</th><th>Starting Time</th><th>Comment</th><th>Additional</th><th>My Comments</th></tr>";
-do
-  {
-    $tID=$myrow["tID"];
-    $tempSql = "SELECT * FROM PROBLEM_ATTEMPT_TRANSACTION WHERE tID = ($tID+1) and command LIKE '%your comment has been recorded%'";
-    $tempResult = mysql_query($tempSql);
-    if(mysql_fetch_array($tempResult))
+  echo "<h2>Comments in problems $extrae$adminnamee, sorted in $order order of $orderBy</h2>\n";
+
+  $sql = "SELECT * FROM PROBLEM_ATTEMPT AS P1,PROBLEM_ATTEMPT_TRANSACTION AS P2 WHERE $adminNamec $extrac $startDatec $endDatec P1.clientID = P2.clientID AND P2.initiatingParty = 'client' AND P2.command LIKE '%\"action\":\"get-help\",\"text\":%' ORDER BY $orderBy $order";
+  
+  $result = mysql_query($sql);
+  if ($myrow = mysql_fetch_array($result)) {
+    echo "<table border=1>";
+    echo "<tr><th>User Name</th><th>Problem</th><th>Section</th><th>Starting Time</th><th>Comment</th><th>Additional</th><th>My Comments</th></tr>";
+    do
       {
-	$tempResult = NULL;
-	$tempSql = NULL;
+	$tID=$myrow["tID"];
+	$tempSql = "SELECT * FROM PROBLEM_ATTEMPT_TRANSACTION WHERE tID = ($tID+1) and command LIKE '%your comment has been recorded%'";
+	$tempResult = mysql_query($tempSql);
+	if(mysql_fetch_array($tempResult))
+	  {
+	    $tempResult = NULL;
+	    $tempSql = NULL;
+	    $userName=$myrow["userName"];
+	    $userProblem=$myrow["userProblem"];
+	    $userSection=$myrow["userSection"];
+	    $startTime=$myrow["startTime"];
+	    $tempCommand1=$myrow["command"];
+	    $tempCommand2 =explode("get-help\",\"text\":\"",$tempCommand1);
+	    $command=explode("\"}",$tempCommand2[1]);
+	    
+	    echo "<tr><td>$userName</td><td>$userProblem</td><td>$userSection</td><td>$startTime</td><td>$command[0]</td><td><a href=\"/web-UI/index.html?s=$userSection&u=$userName&p=$userProblem&e=$extra\" target=\"_blank\">My Analysis</a></td></tr>";
+	  }
+      }
+    while ($myrow = mysql_fetch_array($result));
+    echo "</table>";
+  }
+
+ } else {
+  // Select sessions
+
+  if($adminName==''){
+    $adminNamec = "";
+    $adminNamee = "";
+  } else {
+    $adminNamec = "userName = '$adminName' AND";
+    $adminNamee = " by $adminName";
+  }  
+  if($extra == 'Reviewed'){
+    $extrac = "extra != 0 AND";
+    $extrae = "reviewed";
+  }else if($extra == 'Original'){
+    $extrac = "extra = 0 AND";
+    $extrae = "solved";
+  }else{
+    $extrac = "";
+    $extrae = "solved or reviewed";
+  }
+  if($startDate){
+    $startDatec = "startTime >= '$startDate' AND";
+  } else {
+    $startDatec = "";
+  }
+  if($endDate){
+    $endDatec = "startTime <= '$endDate' AND";
+  } else {
+    $endDatec = "";
+  }
+
+  echo "<h2>Problems $extrae$adminNamee, sorted in $order order of $orderBy</h2>\n";
+
+  $sql = "SELECT * FROM PROBLEM_ATTEMPT WHERE $adminNamec $extrac  $startDatec $endDatec clientID = clientID ORDER BY $orderBy $order";
+
+  // echo "mysql query \"$sql\"\n";
+  
+  $result = mysql_query($sql);
+  if ($myrow = mysql_fetch_array($result)) {
+    echo "<table border=1>\n";
+    echo "<tr><th>User Name</th><th>Problem</th><th>Section</th><th>Starting Time</th><th>Log</th></tr>\n";
+    do
+      {
+	$clientID=$myrow["clientID"];
 	$userName=$myrow["userName"];
 	$userProblem=$myrow["userProblem"];
 	$userSection=$myrow["userSection"];
 	$startTime=$myrow["startTime"];
-	$tempCommand1=$myrow["command"];
-	$tempCommand2 =explode("get-help\",\"text\":\"",$tempCommand1);
-	$command=explode("\"}",$tempCommand2[1]);
-
-	echo "<tr><td>$userName</td><td>$userProblem</td><td>$userSection</td><td>$startTime</td><td>$command[0]</td><td><a href=\"/web-UI/index.html?s=$userSection&u=$userName&p=$userProblem&e=$extra\" target=\"_blank\">My Analysis</a></td></tr>";
+	
+	echo "<tr><td>$userName</td><td>$userProblem</td><td>$userSection</td><td>$startTime</td><td><a href=\"OpenTrace.php?x=$dbuser&sv=$dbserver&pwd=$dbpass&d=$dbname&cid=$clientID\">Session log</a></td></tr>\n";
+       
       }
+    while ($myrow = mysql_fetch_array($result));
+    echo "</table>";
   }
- while ($myrow = mysql_fetch_array($result));
- echo "</table>";
+
  }
 
 mysql_close();
@@ -90,4 +184,9 @@ function UpdateRecord($url){
   }
   oXmlHttp.send(null);
 }
+
+function openTrace($url){  
+  window.open($url);
+}
+
 </script>
