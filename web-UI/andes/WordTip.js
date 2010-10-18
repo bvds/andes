f5d5f49e05ecfcd8b812c8100b14d3e012c49032
Ace Sorensen <asaelh@gmail.com>
2010-10-18 14:29:11 -0700

    Limited results to 5, accessed the current stencil in drawing for server send.

--- web-UI/andes/WordTip.js
+++ web-UI/andes/WordTip.js
@@ -7,15 +7,20 @@ dojo.declare("andes.WordTip", null, {
     //      to show them to the student.
     //stub: null,
     conEdit: null,
+    stencil: null,
     constructor: function(){
         this.conEdit = dojo.byId("conEdit");    
         dojo.connect(this.conEdit, "keydown", this, "textMonitor");
         console.log("I've got conedit now", this.conEdit);
     },
     
+    add: function(obj){
+        this[obj.id] = obj;
+    },
+    
     textMonitor: function(evt){
         if(evt.keyCode == dojo.keys.SPACE || evt.keyCode == dojo.keys.TAB || evt.keyCode == 188){
-		console.log("this=",this);
+	    console.log("this=",this);
             var tx = dojo.trim(this.conEdit.innerHTML);//this.statement.cleanText(conEdit.innerHTML);
             tx = this.removeBreaks(tx);
             var symbol = andes.variablename.parse(tx);
@@ -29,24 +34,55 @@ dojo.declare("andes.WordTip", null, {
     
     removeBreaks: function(txt){
         dojo.forEach(['<br>', '<br/>', '<br />', '\\n', '\\r'], function(br){
-                txt = txt.replace(new RegExp(br, 'gi'), " ");
+            txt = txt.replace(new RegExp(br, 'gi'), " ");
         });
         return txt;
     },
     
-	sendToServer: function(text,symbol){
-	    andes.api.suggestWord({type: "statement", text: text, symbol:symbol});
-	},
-	
-	processResults: function(results){
-		dojo.forEach(results,function(line){
-			console.log("starting line ",line);
-			if(line.action=="next-words"){  
-				console.log("Successfully retrieved tips: ", line.words.join());
-				dijit.showTooltip(line.words.join(),this.conEdit, "above");
-			} else {
-				console.log("no action found");
-			}
-		})
-	}
+    sendToServer: function(text,symbol){
+        /* This retrieves the parentNode.  Currently we're using drawing
+          to get the currentStencil but this may be a better way to do
+          it.  Leaving here for future reference.
+        var parentObj = dojo.attr(this.conEdit.parentNode,"id");
+        console.log("parentOBj: ",parentObj);
+        */
+        var andesTypes = {
+		"dojox.drawing.stencil.Line":"line",
+		"dojox.drawing.stencil.Rect":"rectangle",
+		"dojox.drawing.stencil.Ellipse":"ellipse", // or circle
+		"dojox.drawing.tools.custom.Vector":"vector",
+		"dojox.drawing.tools.custom.Axes":"axes",
+		"dojox.drawing.tools.custom.Equation":"equation",
+		"dojox.drawing.stencil.Image":"graphics",
+		"dojox.drawing.tools.TextBlock":"statement", // or statement.... hmmmm
+		"andes.buttonCombo":"button"
+	};
+        
+        var current = "statement";
+        if(this.drawing){
+            var type = this.drawing.currentStencil.type;
+            current = andesTypes[type];
+        };
+        //console.log("Current-------------------------", current);
+        andes.api.suggestWord({type: current, text: text, symbol:symbol});
+    },
+    
+    processResults: function(results){
+        dojo.forEach(results, function(line){
+            console.log("starting line ",line);
+            if(line.action=="next-words"){
+                dijit.hideTooltip(this.conEdit);
+                if(line.words.length > 1){
+                    var size = Math.min(5, line.words.length);
+                    var wrd = "";
+                    for(var i=0; i<size; i++){
+                        wrd += line.words[i];
+                        wrd += size-1 == i ? "" : ", ";
+                    };
+                    console.log("Successfully retrieved tips: ", line.words.join(), " \nminimized to: ", wrd);
+                    dijit.showTooltip(wrd, this.conEdit, "above");   
+                };
+            };
+        },this);
+    }
 });
