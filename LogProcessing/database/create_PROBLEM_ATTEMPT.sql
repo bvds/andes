a0719d09f0017cbd73cc421c772da5a0cb7e81b9
Brett van de Sande <bvds@asu.edu>
2011-11-09 14:01:50 -0700

    Make extra field in PROBLEM_ATTEMPT into a varchar.

--- LogProcessing/database/create_PROBLEM_ATTEMPT.sql
+++ LogProcessing/database/create_PROBLEM_ATTEMPT.sql
@@ -4,7 +4,7 @@ CREATE TABLE `PROBLEM_ATTEMPT` (
   `clientID` varchar(50) NOT NULL default 'fill in clientID',
   `userProblem` varchar(50) default NULL COMMENT 'The problem the user asks for when communicating with server. May or may not exist. If it exists, it should match up with a given problem name in STUDENT_DATASET',
   `userSection` varchar(50) NOT NULL DEFAULT 'defaultSection' COMMENT 'The section the user is enrolled in. Must match up with a section in CLASS_INFORMATION.',
-  `extra` int(10) DEFAULT 0,
+  `extra` varchar(50) DEFAULT NULL, COMMENT 'Identifiers for multi-user sessions or copied sessions or other such stuff.',
   PRIMARY KEY  (`clientID`),
   KEY `FK_problemstate_classinformation` (`userSection`),
   CONSTRAINT `FK_problemstate_classinformation` FOREIGN KEY (`userSection`) REFERENCES `CLASS_INFORMATION` (`classSection`) ON DELETE CASCADE ON UPDATE CASCADE
