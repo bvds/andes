e98c50c302bfd2f6d6c69bf22adb92451ce8f534
Brett van de Sande <bvds@asu.edu>
2012-01-26 22:56:22 -0700

    Use session-local random number for choosing random-help strategy. Fix bad assoc match which was causing MySQL-related errors.

--- Help/random-help-experiment-setup.sql
+++ Help/random-help-experiment-setup.sql
@@ -20,3 +20,31 @@ INSERT into CLASS_INFORMATION (classSection,description) values ('uwplatt_2Y1305
 INSERT into CLASS_INFORMATION (classSection,description) values ('uwplatt_3n13056a8a6174fbeuwplattl1_','Pawl, physics 2340C2');
 INSERT into CLASS_INFORMATION (classSection,description) values ('uwplatt_8p130495419184f26uwplattl1_','Scaife, Physics 2240');
 INSERT into CLASS_INFORMATION (classSection,description) values ('uwplatt_9047621c019184fdbuwplattl1_','Scaife, Physics 2340');
+
+/*
+  Rerunning sessions through server
+
+sbcl --dynamic-space-size 1000
+
+(rhelp)
+(setf webserver:*debug* nil) 
+(setf *simulate-loaded-server* nil)
+(start-help :db "andes_test" :port 8081)
+(random-help-experiment::set-experiment-probability 0.5)
+
+use andes_test;
+DELETE FROM PROBLEM_ATTEMPT WHERE clientID LIKE '\_%';
+ 
+  May need to run above inserts into CLASS_INFORMATION
+
+
+Steps for getting mysql log file:
+ Stop Andes
+  Restart mysqld to get new binary log file
+ start Andes
+ 
+ls -l -t /var/lib/mysql  # to get name of latest log file
+
+mysqlbinlog --start-datetime="2012-01-25 14:00:00" --stop-datetime="2012-01-25 16:00:00" mysqlLOGb.000028 > /tmp/junk.out
+  
+*/
