4df9fd85a00b93c48259cfc94537f8820ecfb714
Brett van de Sande <bvds@asu.edu>
2012-03-02 21:41:03 -0700

    Some bug fixes for creating new database. (cherry picked from commit cdf92e7ce7df044fc92fe6543a878f052cb26911)

--- LogProcessing/database/create_REVIEWED_PROBLEMS.sql
+++ LogProcessing/database/create_REVIEWED_PROBLEMS.sql
@@ -6,6 +6,6 @@ CREATE TABLE `REVIEWED_PROBLEMS` (
  `radioID` int(10) unsigned DEFAULT 0,
  `myComment` text,
  PRIMARY KEY(`extra`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Base Table corresponding to the set of problems reviewed by the admin'
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Base Table corresponding to the set of problems reviewed by the admin';
 
 GRANT SELECT,INSERT,UPDATE ON REVIEWED_PROBLEMS TO 'open'@'localhost';
