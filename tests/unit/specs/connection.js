000ba03d5344f97756bfe5cbe5bc6e19ca709577
Jason Cline <jason.cline@gmail.com>
2013-12-03 09:11:24 -0800

    [BROW-1230] updated connection to return a promise.  Added test cases to the unit tests.

--- tests/unit/specs/connection.js
+++ tests/unit/specs/connection.js
@@ -7,12 +7,100 @@ describe("Connection", function() {
             done();
         });
 
+
+        it("covert status should handle online", function(done) {
+            var result = Ext.space.Connection._convertStatus({online: "1", type:"WIFI"});
+            expect(result).to.exist;
+            expect(result.online).to.exist;
+            expect(result.online).to.equal(true);
+
+            expect(result.type).to.exist;
+            expect(result.type).to.equal("WIFI");
+
+            expect(result.typeString).to.exist;
+            expect(result.typeString).to.equal(Ext.space.Connection.WIFI);
+
+            done();
+           
+        });
+
+        it("covert status should handle offline", function(done) {
+            var result = Ext.space.Connection._convertStatus({online: "0", type:"WIFI"});
+            expect(result).to.exist;
+            expect(result.online).to.exist;
+            expect(result.online).to.equal(false);
+
+            expect(result.type).to.exist;
+            expect(result.type).to.equal("WIFI");
+
+            expect(result.typeString).to.exist;
+            expect(result.typeString).to.equal(Ext.space.Connection.WIFI);
+
+            done();
+           
+        });
+
+        it("covert status should handle unknown", function(done) {
+            var result = Ext.space.Connection._convertStatus({online: undefined, type:"WIFI"});
+            expect(result).to.exist;
+            expect(result.online).to.exist;
+            expect(result.online).to.equal(false);
+
+            expect(result.type).to.exist;
+            expect(result.type).to.equal("WIFI");
+
+            expect(result.typeString).to.exist;
+            expect(result.typeString).to.equal(Ext.space.Connection.WIFI);
+
+            done();
+           
+        });
+
+        it("covert status should handle online CELL", function(done) {
+            var result = Ext.space.Connection._convertStatus({online: "1", type:"CELL_3G"});
+            expect(result).to.exist;
+            expect(result.online).to.exist;
+            expect(result.online).to.equal(true);
+
+            expect(result.type).to.exist;
+            expect(result.type).to.equal("CELL_3G");
+
+            expect(result.typeString).to.exist;
+            expect(result.typeString).to.equal(Ext.space.Connection.CELL_3G);
+
+            done();
+           
+        });
+
+
+        it("covert status should handle offline CELL", function(done) {
+            var result = Ext.space.Connection._convertStatus({online: "0", type:"CELL_3G"});
+            expect(result).to.exist;
+            expect(result.online).to.exist;
+            expect(result.online).to.equal(false);
+
+            expect(result.type).to.exist;
+            expect(result.type).to.equal("CELL_3G");
+
+            expect(result.typeString).to.exist;
+            expect(result.typeString).to.equal(Ext.space.Connection.CELL_3G);
+
+            done();
+           
+        });
+
         it("should be able to determine whether device is online", function(done) {
-            Ext.space.Connection.getStatus({callback: function(e){
-                  expect(e.online).to.exist;
-                  expect(e.type).to.exist;
+            var result = Ext.space.Connection.getStatus();
+
+            expect(result).to.exist;
+            expect(result.then).to.exist;
+            
+
+            result.then(function(status){
+                  expect(status.online).to.exist;
+                  expect(status.type).to.exist;
                   done();
-            }});
+            });
           
            
         });
